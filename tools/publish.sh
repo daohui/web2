c595616159d65916aa6885a8c0e8fc3dd2ff8e14
Daohui Li <daohui2000@outlook.com>
2017-08-03 11:03:02 -0500

    add publish.sh

---
+++ tools/publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+msg=`git log --format="%H" -n1`
+git checkout gh-pages
+git rm *.css 
+git rm *.js 
+
+for name in `ls dist`; do
+   cp dist/$name $name
+   git add $name
+done
+cp index.html 404.html
+git add 404.html
+
+git commit -m $msg && git push && git checkout master
+
