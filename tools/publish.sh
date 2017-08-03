cdefa6bbf4df914bbff7c03b10c5c86c8a59b213
Daohui Li <daohui2000@outlook.com>
2017-08-03 11:16:50 -0500

    drop elements from gh-pages branch

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-msg=`git log --format="%H" -n1`
-git checkout gh-pages
-git rm *.css 
-git rm *.js 
+publish() {
+   msg=`git log --format="%H" -n1`
+   git checkout gh-pages
+   git rm *.css 
+   git rm *.js 
+   
+   for name in `ls dist`; do
+      cp dist/$name $name
+      git add $name
+   done
+   cp index.html 404.html
+   git add 404.html
+   
+   git commit -m $msg && git push --force && git checkout master
+}
 
-for name in `ls dist`; do
-   cp dist/$name $name
-   git add $name
-done
-cp index.html 404.html
-git add 404.html
-
-git commit -m $msg && git push && git checkout master
-
+if ng build --base-href /web2/ -prod -aot=false; then
+   publish
+fi
